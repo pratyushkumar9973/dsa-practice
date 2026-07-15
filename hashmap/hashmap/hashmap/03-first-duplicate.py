622bf220f053c3918dcdf9ae06deeb17e1afc936
Pratyush kumar <pratyushdubey006@gmail.com>
2026-07-15 22:55:02 +0530

    Fix nested hashmap folder structure

--- hashmap/hashmap/hashmap/03-first-duplicate.py
+++
@@ -1,9 +0,0 @@
-def first_duplicate(word):
-    seen = {}
-    for i, letter in enumerate(word):
-        if letter in seen:
-            return letter
-        seen[letter] = i
-    return None
-
-print(first_duplicate("hello"))
