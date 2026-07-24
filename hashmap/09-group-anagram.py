880b7847d7c35ae4d3dfe1bdec59be265fdee40b
Pratyush kumar <pratyushdubey006@gmail.com>
2026-07-25 01:50:19 +0530

    Create 09-group-anagram.py

---
+++ hashmap/09-group-anagram.py
@@ -0,0 +1,10 @@
+def groupAnagrams(strs):
+   anagram_map = {}
+
+    for s in strs:
+        sorted_str = ''.join(sorted(s))
+        if sorted_str not in anagram_map:
+            anagram_map[sorted_str] = []
+        anagram_map[sorted_str].append(s)
+
+    return list(anagram_map.values())
